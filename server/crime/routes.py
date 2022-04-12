905b432a15996992326c259f7b97c15c015b2e6d
Abdulsalam Oetayo <adeodetayo92@gmail.com>
2022-04-12 09:16:58 -0400

    Feat, Fix: different routes for each statistic

--- server/crime/routes.py
+++ server/crime/routes.py
@@ -1,16 +1,26 @@
+from json import loads
 from bson.json_util import dumps
 from flask import Flask, jsonify, request, redirect
 from app import app, db
 
-collection = db["crime"] #crime table
+crime_collection = db["crime"]  # crime table
+crime_statistics_collection = db["crime_statistics"]  # crime statistics table
 
 # GET requests
-@app.route("/api/crimes", methods=["GET"])
-def crimes():
-    weapons = collection.distinct("Weapon")
+# Gets all the calculated crime statistics, from the database
+@app.route("/api/crimes/statistics", methods=["GET"])
+def crimes_statistics():
+    cursor = crime_statistics_collection.find({}, {"_id": False})
+    return jsonify(list(cursor)), 200
+
+
+# Calculates the crime statistic for weapons distribution, from the database
+@app.route("/api/crimes/statistics/weapons", methods=["GET"])
+def weapons_distribution():
+    weapons = crime_collection.distinct("Weapon")
     data = []
     for type in weapons:
-        number_of_crimes = collection.count_documents({"Weapon": type})
+        number_of_crimes = crime_collection.count_documents({"Weapon": type})
         data.append({"type": type, "value": number_of_crimes})
 
     return {"tag": "bar", "title": "Weapons Distribution", "data": data}, 200

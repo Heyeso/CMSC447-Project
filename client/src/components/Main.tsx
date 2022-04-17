465841c88353b606276d881feacf237de88ff833
Stephen Vaudreuil <stephev1@umbc.edu>
2022-04-17 13:31:06 -0400

    Map can be placed in a card

--- client/src/components/Main.tsx
+++ client/src/components/Main.tsx
@@ -101,7 +101,17 @@ function Main({ setCurrentRoute }: Props) {
               setCurrentRoute("Test Map");
             }}
           >
-            
+            <MapContainer center={[39.29, -76.61]} zoom={13} height={300} >
+              <TileLayer
+                //attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[39.29, -76.61]}>
+                <Popup>
+                  A pretty CSS3 popup. <br /> Easily customizable.
+                </Popup>
+              </Marker>
+            </MapContainer>
           </DataCardView>
         </Suspense> */}
         <Suspense fallback={<div>loading</div>}>

f32628d9910761d16966e2a9af3012d3412f2089
KatieK80933 <im80933@umbc.edu>
2022-05-04 12:18:06 -0400

    Filtering

--- server/crime/routes.py
+++ server/crime/routes.py
@@ -16,7 +16,7 @@ GROUPS = {'weapons': 'Weapon', 'weekdays': 'weekdays', 'hours': 'hours', 'dates'
 PROJECTIONS = {'weekdays': 'dayOfWeek', 'hours': 'hour', 'dates': 'dateToString', 'months': 'month', 'years': 'year'}
 
 # crime and crime stats tables
-crime_collection = db["crime"]
+crime_collection = db["crimes"]
 crime_statistics_collection = db["crime_statistics"]
 
 # GET requests
@@ -26,10 +26,10 @@ def crimes_statistics():
     cursor = crime_statistics_collection.find({}, {"_id": False})
     return jsonify(list(cursor)), 200
 
-# Calculates the crime statistic for all distributions from the database
+# Calculates the crime statistic for all distributions from the database, these routes do not use match and type (those are for filtering)
 @app.route("/api/crimes/statistics/<selection>", defaults={'tag': CHARTS[0]}, methods=["GET"])
 @app.route("/api/crimes/statistics/<selection>/<tag>", methods=["GET"])
-def distribution(selection, tag):
+def distribution(selection, tag, match=None, type=None):
     # data will hold dictionaries for each data entry
     data = []
     # command will hold the query
@@ -55,8 +55,35 @@ def distribution(selection, tag):
         command = [{"$group": {"_id": {TITLES[selection]: "$" + GROUPS[selection]}, "count": {"$sum": 1}}}]
     point = TITLES[selection]
 
+    # Handle if there is a filter
+    if (match != None) and (type != None):
+        group = []
+        # There are 4 options for data pairs: date/date, non-date/date, date/non-date, and non-date/non-date
+        # Date/Date -           project date 1 and date 2, match date 2, group by date 1
+        # Non-Date/Date -       project date (not in command) and include non-date, match date, group by non-date
+        # Date/Non-Date -       project date (already in command) and include non-date, match non-date, group by date
+        # Non-Date/Non-Date -   no projections, match non-date 2, group by non-date 1
+        if match in PROJECTIONS.keys():
+            type = int(type)
+            # Ex. Date/Date - Selection is Weekdays, but match is Months, need the two projections, then the match, then the group
+            if selection in PROJECTIONS.keys():
+                # Add the second projection
+                command[0]['$project'][match] = {"$" + PROJECTIONS[match]: "$CrimeDateTime"}
+                group = [command[1]]
+                command = [command[0]] + [{'$match': {match: type}}] + group
+            # Ex. Non-Date/Date - Selection is Weapons, but match is Months, need two projections (1 for motnhs, then include weapons), then a match, then a group
+            else:
+                command = [{'$project': {match: {"$" + PROJECTIONS[match]: "$CrimeDateTime"}, GROUPS[selection]: 1}}] + [{'$match': {match: type}}] + command
+        # Ex. Date/Non-Date - Selection is Months, but match is Weapons, need the months projection, then the match, then the group
+        elif selection in PROJECTIONS.keys():
+            command[0]['$project'][match] = 1
+            command = [command[0]] + [{'$match': {match: type}}] + [command[1]]
+        # Ex. Non-Date/Non-Date - Selection is Descriptions, but match is Weapons, need the match then the group
+        else:
+            command = [{'$match': {match: type}}] + command
 
     # Aggregate the command
+    print(command)
     cursor = crime_collection.aggregate(command)
     
     # Go through documents
@@ -87,72 +114,20 @@ def distribution(selection, tag):
 
 
 # New Route for Expand Page filters
-#  TODO: Refactor to work for all statistics type
-@app.route("/api/crimes/statistics/expand/weapons/<type>", methods=["GET"])
-def expand_statistics(selection, type):
+# To filter: weapons/FIRE or descriptions/ or districts/, weekdays/1-7 (Sunday-Saturday), hours/0-23 (12am-11pm), months/1-12 (January-December), years/2020
+@app.route("/api/crimes/statistics/expand/<match>/<type>", methods=["GET"])
+def expand_statistics(match, type):
     data = []
 
-    for item in SELECTIONS:
-        if item != "weapons" and item != "dates":
-            result, code = expand_distribution(item, "bar", { "$match": {"Weapon": type} })
+    # Need to filter for every selection except the one to match, and dates
+    for selection in SELECTIONS:
+        if selection != match and selection != "dates":
+            # fix the match name to be able to group
+            match_group = GROUPS[match]
+            # find the filtered distribution and append the result
+            result, code = distribution(selection, "bar", match_group, type)
             if code != 200:
                 abort(404)
             data.append(result)
             
     return jsonify(data), 200
-
-#  TODO: Needs refactoring, Improving
-def expand_distribution(selection, tag, filter):
-    # data will hold dictionaries for each data entry
-    data = []
-    # command will hold the query
-    command = []
-    # point will hold the _id call
-    point = ""
-
-    # Bad tags - should throw a 404
-    if tag not in CHARTS:
-        abort(404)
-    # Bad selections - should throw a 404
-    if selection not in SELECTIONS:
-        abort(404)
-
-    # Create commands and title point
-    if selection in PROJECTIONS.keys() and selection != 'dates':
-        command = [filter, {'$project': {selection: {"$" + PROJECTIONS[selection]: "$CrimeDateTime"}}},
-                   {'$group': {"_id": {TITLES[selection]: "$" + GROUPS[selection]}, "count": {'$sum': 1}}}]
-    elif selection == 'dates':
-        command = [filter, {'$project': {"dates": {"$dateToString": {"format": "%Y-%m-%d", "date": "$CrimeDateTime"}}}},
-                   {"$group": {"_id": {"Date": "$dates"}, "count": {"$sum": 1}}}]
-    else:
-        command = [filter, {"$group": {"_id": {TITLES[selection]: "$" + GROUPS[selection]}, "count": {"$sum": 1}}}]
-    point = TITLES[selection]
-
-    # Aggregate the command
-    print(command)
-    cursor = crime_collection.aggregate(command)
-    
-    # Go through documents
-    all_docs = sorted(list(cursor), key = lambda i:i['_id'][point])
-    item = ""
-    for document in all_docs:
-        # Need to convert nums to weekdays, hours need ":00", nums to months if these are the selection
-        if selection == 'weekdays':
-            item = WEEKDAYS[document["_id"][point] - 1]
-        elif selection == 'hours':
-            item = str(document["_id"][point]) + ':00'
-        elif selection == 'months':
-            item = MONTHS[document["_id"][point] - 1]
-        else:
-            item = str(document["_id"][point])
-    
-        # Check for valid data
-        if (item != "") and (item != "NA"):
-            data.append(
-                {
-                    "type": item.replace("_", " ").title(),
-                    "value": document["count"],
-                }
-            )
-
-    return {"tag": tag, "title": point +  " Distribution", "data": data}, 200
